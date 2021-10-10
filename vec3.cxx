5dbbf2fc091531853b94edaa56f0f1cdfd8c13bb
wermos <63574588+wermos@users.noreply.github.com>
2021-10-10 11:22:10 +0530

    Minor code style changes.

--- vec3.cxx
+++ vec3.cxx
@@ -2,7 +2,7 @@
 // vec3 Utility Functions
 
 inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
-    return out << v.m_e[0] << ' ' << v.m_e[1] << ' ' << v.m_e[2];
+    return out << v.m_e[0] << " " << v.m_e[1] << " " << v.m_e[2];
 }
 
 inline vec3 operator+(const vec3& u, const vec3& v) {
@@ -18,7 +18,7 @@ inline vec3 operator*(const vec3& u, const vec3& v) {
 }
 
 inline vec3 operator*(double t, const vec3& v) {
-    return vec3(t*v.m_e[0], t*v.m_e[1], t*v.m_e[2]);
+    return vec3(t * v.m_e[0], t * v.m_e[1], t * v.m_e[2]);
 }
 
 inline vec3 operator*(const vec3& v, double t) {
