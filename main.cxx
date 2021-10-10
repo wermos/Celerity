192bc11a1e3e55b88d3379e2cc97ebb53aad7381
wermos <63574588+wermos@users.noreply.github.com>
2021-10-10 11:46:20 +0530

    Updated `main` to use all the new functions and classes.

--- main.cxx
+++ main.cxx
@@ -2,12 +2,36 @@
 
 #include "vec3.hpp"
 #include "color.hpp"
+#include "ray.hpp"
+
+color rayColor(const ray& r) {
+    vec3 unitDirection = unitVector(r.direction());
+	//normalizing makes all the coordinates vary from [-1, 1] (inclusive)
+
+    auto t = 0.5 * (unitDirection.y() + 1.0);
+	//This is a trick to make the y value vary from [0, 1]
+
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+	//This is a linear combination of the start and end colors to make a
+	//smooth and linear color gradient.
+}
 
 int main() {
 
     // Image
-    const int imageWidth = 256;
-    const int imageHeight = 256;
+    const auto aspectRatio = 16.0 / 9.0;
+    const int imageWidth = 400;
+    const int imageHeight = static_cast<int>(imageWidth / aspectRatio);
+
+    // Camera
+    auto viewportHeight = 2.0;
+    auto viewportWidth = aspectRatio * viewportHeight;
+    auto focalLength = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewportWidth, 0, 0);
+    auto vertical = vec3(0, viewportHeight, 0);
+    auto lowerLeftCorner = origin - (horizontal / 2) - (vertical / 2) - vec3(0, 0, focalLength);
 
     // Render
     std::cout << "P3\n" << imageWidth << " " << imageHeight << "\n255\n";
@@ -24,7 +48,12 @@ int main() {
         std::cerr << "\rScanlines remaining: " << j << " " << std::flush;
 
         for (int i = 0; i < imageWidth; ++i) {
-            color pixelColor(double(i) / (imageWidth - 1), double(j) / (imageHeight - 1), 0.25);
+			auto u = double(i) / (imageWidth - 1);
+            auto v = double(j) / (imageHeight - 1);
+
+            ray r(origin, lowerLeftCorner + u * horizontal + v * vertical - origin);
+            color pixelColor = rayColor(r);
+
             std::cout<< pixelColor;
         }
     }
