84140c34559ca4af8edc04bff3e233cfe7dc03a2
wermos <63574588+wermos@users.noreply.github.com>
2021-10-14 20:45:10 +0530

    Included the stb_image_write.h file in main.cxx.

--- main.cxx
+++ main.cxx
@@ -1,8 +1,14 @@
 #include <iostream>
 
+#ifndef STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#endif
+
 #include "vec3.hpp"
 #include "color.hpp"
 #include "ray.hpp"
+
+#include "stb_image_write.h"
 
 color rayColor(const ray& r) {
     vec3 unitDirection = unitVector(r.direction());
