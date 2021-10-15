df9fb01afcf470c6973d0c6d6b2a81a0755a044a
wermos <63574588+wermos@users.noreply.github.com>
2021-10-15 17:03:50 +0530

    Updated a comment.

--- main.cxx
+++ main.cxx
@@ -41,14 +41,16 @@ int main() {
 
     // Render
     std::cout << "P3\n" << imageWidth << " " << imageHeight << "\n255\n";
-    //PPM format starts with
-    //P3
-    //<width> <height>
-    //<max value of color>
-    //where "P3" tells the viewer that the colors are in ASCII
-    //<width> tells the viewer how many columns there are in the picture,
-    //<height> tells the viewer how many rows, and
-    //<max value of color> tells the viewer how to normalize the color values
+    /**
+     * P3
+     * <width> <height>
+     * <max value of color>
+	 *
+     * where "P3" tells the viewer that the colors are in ASCII
+     * <width> tells the viewer how many columns there are in the picture,
+     * <height> tells the viewer how many rows, and
+     * <max value of color> tells the viewer how to normalize the color values
+     */
 
     for (int j = imageHeight - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << " " << std::flush;
