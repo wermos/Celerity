ba29a05bdaac842e23c3cad84155e02dc1961aad
wermos <63574588+wermos@users.noreply.github.com>
2021-10-21 00:53:40 +0530

    Added a sphere class.

---
+++ Tracer/include/sphere.hpp
@@ -0,0 +1,20 @@
+#ifndef SPHERE_HPP
+#define SPHERE_HPP
+
+#include "hittable.hpp"
+#include "ray.hpp"
+
+class sphere requires HittableObject<sphere> {
+	public:
+        sphere() = default;
+        sphere(point3 center, double radius) : m_center(center), m_radius(radius)
+		{}
+
+        hit(const ray& r, double t_min, double t_max, hitRecord& rec) const;
+
+    private:
+        point3 m_center;
+        double m_radius;
+};
+
+#endif // SPHERE_HPP
