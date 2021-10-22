5e60c9ec93d24b7b02ac2fbd97819ab344a09fd9
wermos <63574588+wermos@users.noreply.github.com>
2021-10-22 23:08:58 +0530

    Added in the return type of the hit function.

--- Tracer/include/sphere.hpp
+++ Tracer/include/sphere.hpp
@@ -2,15 +2,17 @@
 #define SPHERE_HPP
 
 #include "hittable.hpp"
+#include "vec3.hpp"
 #include "ray.hpp"
 
 class sphere requires HittableObject<sphere> {
 	public:
-        sphere() = default;
+		sphere() = default;
+
         sphere(point3 center, double radius) : m_center(center), m_radius(radius)
 		{}
 
-        hit(const ray& r, double t_min, double t_max, hitRecord& rec) const;
+        bool hit(const ray& r, double t_min, double t_max, hitRecord& rec) const;
 
     private:
         point3 m_center;
