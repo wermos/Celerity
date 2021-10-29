437e0a1e1512b3af628e19d9f00d5abaa096e9ab
wermos <63574588+wermos@users.noreply.github.com>
2021-10-29 13:30:55 +0530

    Incorporated the Material class into the Sphere class and redid the `#include`s.

--- Tracer/include/shapes/sphere.hpp
+++ Tracer/include/shapes/sphere.hpp
@@ -1,14 +1,22 @@
 #ifndef SPHERE_HPP
 #define SPHERE_HPP
 
+#include <memory>
+
 #include "hittable.hpp"
+#include "hitRecord.hpp"
+
+#include "material.hpp"
+
 #include "vec3.hpp"
 #include "ray.hpp"
 
 class Sphere : public HittableObject {
 	public:
+		Sphere() = default;
 
-        Sphere(point3 center, double radius) : m_center(center), m_radius(radius) {};
+        Sphere(point3 center, double radius, std::shared_ptr<Material> material) :
+			m_center(center), m_radius(radius), m_material(material) {};
 
         bool hit(
 			const ray& r, double tMin, double tMax, HitRecord& rec) const override;
@@ -16,6 +24,7 @@ class Sphere : public HittableObject {
     private:
         point3 m_center;
         double m_radius;
+		std::shared_ptr<Material> m_material;
 };
 
 #endif // SPHERE_HPP
