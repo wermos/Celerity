c2736230ebf49ac2c056c15e4c45012b06d544ff
wermos <63574588+wermos@users.noreply.github.com>
2021-12-01 15:21:37 +0530

    Added an if clause to catch a degenerate scatter direction.

--- Tracer/src/materials/lambertian.cxx
+++ Tracer/src/materials/lambertian.cxx
@@ -3,10 +3,16 @@
 #include "vec3.hpp"
 
 bool Lambertian::scatter(
-    const ray& in, const HitRecord& record, color& attenuation, ray& scattered
+    	const ray& in, const HitRecord& record, color& attenuation, ray& scattered
     ) const {
-            auto scatterDirection = record.normal + vec3::randomInUnitSphere();
-            scattered = ray(record.point, scatterDirection);
-            attenuation = m_albedo;
-            return true;
+    auto scatterDirection = record.normal + vec3::randomInUnitSphere();
+
+	// Catch degenerate scatter direction
+            if (scatterDirection.nearZero())
+                scatterDirection = record.normal;
+
+    scattered = ray(record.point, scatterDirection);
+    attenuation = m_albedo;
+
+    return true;
 }
