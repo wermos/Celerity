8d732e20b2d8ce730f948b3af1c9237b42d217ba
wermos <63574588+wermos@users.noreply.github.com>
2022-01-24 14:11:31 +0530

    Added support for fuzzy reflections in the metal material.

--- Celerity/include/materials/metal.hpp
+++ Celerity/include/materials/metal.hpp
@@ -10,15 +10,16 @@
 
 
 class Metal : public Material {
-    public:
-        Metal(const color& a) : m_albedo(a) {}
+	public:
+		Metal(const color& a, Float fuzz) : m_albedo(a), m_fuzz(fuzz < 1 ? fuzz : 1) {}
 
-        virtual bool scatter(
-            const ray& in, const HitRecord& record, color& attenuation, ray& scattered
-        ) const override;
+		virtual bool scatter(
+			const ray& in, const HitRecord& record, color& attenuation, ray& scattered
+		) const override;
 
-    public:
-        color m_albedo;
+	private:
+		color m_albedo;
+		Float m_fuzz;
 };
 
 #endif // METAL_HPP
--- Celerity/src/materials/metal.cxx
+++ Celerity/src/materials/metal.cxx
@@ -3,11 +3,11 @@
 #include "vec3.hpp"
 
 bool Metal::scatter(
-    	const ray& in, const HitRecord& record, color& attenuation, ray& scattered
-    ) const {
+		const ray& in, const HitRecord& record, color& attenuation, ray& scattered
+	) const {
 	vec3 reflected = vec3::reflect(unitVector(in.direction()), record.normal);
 
-	scattered = ray(record.point, reflected);
+	scattered = ray(record.point, reflected + m_fuzz * vec3::randomInUnitSphere());
 	attenuation = m_albedo;
 
 	return (dot(scattered.direction(), record.normal) > 0);
