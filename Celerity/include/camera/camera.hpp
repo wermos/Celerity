b9d405f00c848164de770da8f589679ad32212b2
wermos <63574588+wermos@users.noreply.github.com>
2022-02-08 12:21:41 +0530

    Improved the `Camera` class even more.
    Added customiazle `lookAt` and `lookFrom` vectors to the camera class.

--- Celerity/include/camera/camera.hpp
+++ Celerity/include/camera/camera.hpp
@@ -10,7 +10,8 @@
 
 class Camera {
 	public:
-		Camera(double vfov, double aspectRatio) {
+		Camera(point3 lookFrom, point3 lookAt, vec3 viewUp,
+			   double vfov, double aspectRatio) {
 			// vfov = vertical field-of-view, in degrees
 			const auto theta = degreesToRadians(vfov);
 			const auto h = tan(theta / 2);
@@ -19,14 +20,20 @@ class Camera {
 
 			const auto focalLength = 1.0;
 
-			m_origin = point3(0, 0, 0);
-			m_horizontal = vec3(viewportWidth, 0.0, 0.0);
-			m_vertical = vec3(0.0, viewportHeight, 0.0);
-			m_lowerLeftCorner = m_origin - (m_horizontal / 2) - (m_vertical / 2) - vec3(0, 0, focalLength);
+			// Now we have a complete orthonormal basis (u,v,w) to describe
+			// our camera’s orientation.
+			auto w = unitVector(lookFrom - lookAt);
+			auto u = unitVector(cross(viewUp, w));
+			auto v = cross(w, u);
+
+			m_origin = lookFrom;
+			m_horizontal = viewportWidth * u;
+			m_vertical = viewportHeight * v;
+			m_lowerLeftCorner = m_origin - m_horizontal / 2 - m_vertical / 2 - w;
 		}
 
-		const ray getRay(Float u, Float v) const {
-			return {m_origin, m_lowerLeftCorner + (u * m_horizontal) + (v * m_vertical) - m_origin};
+		const ray getRay(Float s, Float t) const {
+			return {m_origin, m_lowerLeftCorner + (s * m_horizontal) + (t * m_vertical) - m_origin};
 		}
 
 	private:
--- Celerity/src/main.cxx
+++ Celerity/src/main.cxx
@@ -38,28 +38,21 @@ int main() {
 		constexpr std::size_t maxRayDepth = 50;
 
 		// World
-		auto R = cos(std::numbers::pi/4);
 		HittableList world;
 
-		// auto groundMaterial = std::make_shared<Lambertian>(color(0.8, 0.8, 0.0));
-		// auto centerMaterial = std::make_shared<Lambertian>(color(0.1, 0.2, 0.5));
-		// auto leftMaterial   = std::make_shared<Dielectric>(1.5);
-		// auto rightMaterial  = std::make_shared<Metal>(color(0.8, 0.6, 0.2), 0.0);
+		auto groundMaterial = std::make_shared<Lambertian>(color(0.8, 0.8, 0.0));
+		auto centerMaterial = std::make_shared<Lambertian>(color(0.1, 0.2, 0.5));
+		auto leftMaterial   = std::make_shared<Dielectric>(1.5);
+		auto rightMaterial  = std::make_shared<Metal>(color(0.8, 0.6, 0.2), 0.0);
 
-		// world.add(std::make_shared<Sphere>(point3( 0.0, -100.5, -1.0), 100.0, groundMaterial));
-		// world.add(std::make_shared<Sphere>(point3( 0.0,    0.0, -1.0),   0.5, centerMaterial));
-		// world.add(std::make_shared<Sphere>(point3(-1.0,    0.0, -1.0),   0.5, leftMaterial));
-		// world.add(std::make_shared<Sphere>(point3(-1.0,    0.0, -1.0),  -0.4, leftMaterial));
-		// world.add(std::make_shared<Sphere>(point3( 1.0,    0.0, -1.0),   0.5, rightMaterial));
-
-		auto leftMaterial  = std::make_shared<Lambertian>(color(0, 0, 1));
-		auto rightMaterial = std::make_shared<Lambertian>(color(1, 0, 0));
-
-		world.add(std::make_shared<Sphere>(point3(-R, 0, -1), R, leftMaterial));
-		world.add(std::make_shared<Sphere>(point3( R, 0, -1), R, rightMaterial));
+		world.add(std::make_shared<Sphere>(point3( 0.0, -100.5, -1.0), 100.0, groundMaterial));
+		world.add(std::make_shared<Sphere>(point3( 0.0,    0.0, -1.0),   0.5, centerMaterial));
+		world.add(std::make_shared<Sphere>(point3(-1.0,    0.0, -1.0),   0.5, leftMaterial));
+		world.add(std::make_shared<Sphere>(point3(-1.0,    0.0, -1.0),  -0.4, leftMaterial));
+		world.add(std::make_shared<Sphere>(point3( 1.0,    0.0, -1.0),   0.5, rightMaterial));
 
 		// Camera
-		Camera camera(90.0, aspectRatio);
+		Camera camera(point3(-2, 2, 1), point3(0, 0, -1), vec3(0, 1, 0), 90, aspectRatio);
 
 		//Initialize file writers
 		// Making a PPM while using multiple threads is a massive pain and not worth it.
