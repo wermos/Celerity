ff2a31919c7bd41ffb29ba2786eee04faa584974
wermos <63574588+wermos@users.noreply.github.com>
2022-06-18 22:28:49 +0530

    Added a comment explaining the `CMATH_CONSTEXPR` macro's purpose.

--- shared/constexpr.hpp
+++ shared/constexpr.hpp
@@ -4,10 +4,22 @@
 #include <cstddef> // for the libstdc++ macros (if any)
 
 /**
- * TODO: Fill this out.
+ * In C++20, the functions in the cmath header are not required to be marked
+ * as constexpr. However, if you have a recent enough version of libstdc++
+ * (which is the default C++ Standard Library implementation on Linux),
+ * it is marked as constexpr anyways (as an extension of sorts).
+ *
+ * However, libc++ (LLVM's implementation of the Standard Library), and
+ * Microsoft's implementation both have the cmath functions as runtime
+ * functions (i.e. as functions which cannot be invoked in a constant
+ * expression context.)
+ *
+ * For this reason, if the Standard Library implementation we are using is
+ * GNU's, then we mark the functions which use cmath functions in the
+ * implmentation as constexpr, and otherwise we don't.
 */
 #if defined(__GLIBCXX__) && __GLIBCXX__ >= 20210923L
- 	/* Using GNU GCC libstdc++, so using also its STL implementation */
+ 	/* Using GNU GCC libstdc++, so also using its STL implementation */
 	#define CMATH_CONSTEXPR constexpr
 #else
 	#define CMATH_CONSTEXPR
