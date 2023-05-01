7dfe8d556e5d5d8ca049b4b8420bbf8923a5821b
wermos <63574588+wermos@users.noreply.github.com>
2023-05-01 21:07:45 +0530

    Changed the named constructor call to a braced-init-list constructor call.

--- utilities/include/math/vec3.hpp
+++ utilities/include/math/vec3.hpp
@@ -174,9 +174,9 @@ class alignas(16) vec3 {
 		}
 
 		constexpr static vec3 cross(const vec3& u, const vec3& v) {
-			return vec3(u.m_e[1] * v.m_e[2] - u.m_e[2] * v.m_e[1],
-						u.m_e[2] * v.m_e[0] - u.m_e[0] * v.m_e[2],
-						u.m_e[0] * v.m_e[1] - u.m_e[1] * v.m_e[0]);
+			return {u.m_e[1] * v.m_e[2] - u.m_e[2] * v.m_e[1],
+					u.m_e[2] * v.m_e[0] - u.m_e[0] * v.m_e[2],
+					u.m_e[0] * v.m_e[1] - u.m_e[1] * v.m_e[0]};
 		}
 
 		CMATH_CONSTEXPR static vec3 unitVector(vec3 v) {
