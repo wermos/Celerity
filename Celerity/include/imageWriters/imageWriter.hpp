159c8d4c2c7b647d4cfb1c68e48d4347dd0f9ea3
wermos <63574588+wermos@users.noreply.github.com>
2024-02-27 06:28:52 +0530

    Changed the `int`s in the `imageWriter` to `std::size_t`.

--- Celerity/include/imageWriters/imageWriter.hpp
+++ Celerity/include/imageWriters/imageWriter.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <iostream>
+#include <cstddef>
 #include <string>
 
 #include "color.hpp"
@@ -8,7 +8,7 @@
 
 class imageWriter {
 	public:
-		imageWriter(const int imageWidth, const int imageHeight);
+		imageWriter(const std::size_t imageWidth, const std::size_t imageHeight);
 
 		void writeToPNGBuffer(const color c) {
 			m_pngData[m_pngDataCounter++] = static_cast<uint8_t>(c.r());
@@ -16,10 +16,10 @@ class imageWriter {
 			m_pngData[m_pngDataCounter++] = static_cast<uint8_t>(c.b());
 		}
 
-		void writeToPNGBuffer(int index, const color c) {
-			m_pngData[index++] = static_cast<uint8_t>(c.r());
-			m_pngData[index++] = static_cast<uint8_t>(c.g());
-			m_pngData[index] = static_cast<uint8_t>(c.b());
+		void writeToPNGBuffer(std::size_t idx, const color c) {
+			m_pngData[idx++] = static_cast<uint8_t>(c.r());
+			m_pngData[idx++] = static_cast<uint8_t>(c.g());
+			m_pngData[idx] = static_cast<uint8_t>(c.b());
 		}
 
 		void writeToJPGBuffer(const color c) {
@@ -28,39 +28,40 @@ class imageWriter {
 			m_jpgData[m_jpgDataCounter++] = static_cast<uint8_t>(c.b());
 		}
 
-		void writeToJPGBuffer(int index, const color c) {
-			m_jpgData[index++] = static_cast<uint8_t>(c.r());
-			m_jpgData[index++] = static_cast<uint8_t>(c.g());
-			m_jpgData[index] = static_cast<uint8_t>(c.b());
+		void writeToJPGBuffer(std::size_t idx, const color c) {
+			m_jpgData[idx++] = static_cast<uint8_t>(c.r());
+			m_jpgData[idx++] = static_cast<uint8_t>(c.g());
+			m_jpgData[idx] = static_cast<uint8_t>(c.b());
 		}
 
-		//TODO: Explore the stbi_write_png_func and stbi_write_jpg_func functions
-		//which might not require having the entire image in memory while writing
-		//it.
+		// TODO: Explore the stbi_write_png_func and stbi_write_jpg_func functions
+		// which might not require having the entire image in memory while writing
+		// it.
 		int writePNG() {
-			return stbi_write_png(m_pngFileName.c_str(), m_imageWidth,
-								  m_imageHeight, m_pngComp, m_pngData, m_strideInBytes);
+			return stbi_write_png(m_pngFileName.c_str(), static_cast<int>(m_imageWidth),
+								  static_cast<int>(m_imageHeight), static_cast<int>(m_pngComp), m_pngData, m_strideInBytes);
 		}
 
 		int writeJPG() {
-			return stbi_write_jpg(m_jpgFileName.c_str(), m_imageWidth,
-								  m_imageHeight, m_jpgComp, m_jpgData, 80);
+			return stbi_write_jpg(m_jpgFileName.c_str(), static_cast<int>(m_imageWidth),
+								  static_cast<int>(m_imageHeight), static_cast<int>(m_jpgComp), m_jpgData, 80);
 		}
 
 		~imageWriter();
 	private:
-		//Image File Data
-		const int m_jpgComp = 3; //number of channels in the JPG image
-		const int m_pngComp = 3; //number of channels in the PNG image
-		const int m_imageWidth, m_imageHeight;
+		// Image File Data
+		static constexpr std::size_t m_jpgComp = 3; // number of channels in the JPG image
+		static constexpr std::size_t m_pngComp = 3; // number of channels in the PNG image
+		const std::size_t m_imageWidth;
+		const std::size_t m_imageHeight;
 
-		const std::string m_jpgFileName = "pic.jpg";
-		const std::string m_pngFileName = "pic.png";
+		static constexpr std::string m_jpgFileName{"pic.jpg"};
+		static constexpr std::string m_pngFileName{"pic.png"};
 		const int m_strideInBytes;
 
-		//Constant pointer to non-const data
+		// Constant pointer to non-const data
 		uint8_t* const m_jpgData;
 		uint8_t* const m_pngData;
 
-		int m_jpgDataCounter, m_pngDataCounter;
+		std::size_t m_jpgDataCounter, m_pngDataCounter;
 };
--- Celerity/src/imageWriters/imageWriter.cxx
+++ Celerity/src/imageWriters/imageWriter.cxx
@@ -1,8 +1,8 @@
 #include "imageWriters/imageWriter.hpp"
 
-imageWriter::imageWriter(const int imageWidth, const int imageHeight) :
+imageWriter::imageWriter(const std::size_t imageWidth, const std::size_t imageHeight) :
 	m_imageWidth(imageWidth), m_imageHeight(imageHeight),
-	m_strideInBytes(m_imageWidth * m_pngComp),
+	m_strideInBytes(static_cast<int>(m_imageWidth * m_pngComp)),
 	m_jpgData(new uint8_t[m_imageWidth * m_imageHeight * m_jpgComp]),
 	m_pngData(new uint8_t[m_imageWidth * m_imageHeight * m_pngComp]),
 	m_jpgDataCounter(0), m_pngDataCounter(0)
