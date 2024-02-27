164d60a2d8feee562d1b39a72416fb85a65bc646
wermos <63574588+wermos@users.noreply.github.com>
2024-02-27 12:03:57 +0530

    Added a `.clang-format`.

--- Celerity/include/renderer/renderer.hpp
+++ Celerity/include/renderer/renderer.hpp
@@ -1,43 +1,25 @@
 #pragma once
 
 // STL includes
-#include <thread> // For std::thread
-#include <atomic> // For std:atomic<T>
-#include <syncstream> // For std::osyncstream
+#include <atomic>      // For std:atomic<T>
 
-// Math includes
-#include "vec3.hpp"
-#include "color.hpp"
-#include "ray.hpp"
-
-// Object-related includes
-#include "shapes/sphere.hpp"
-
-#include "shared/hittable.hpp"
 #include "shared/hittableList.hpp"
-
-// Material-related includes
-#include "materials/lambertian.hpp"
-#include "materials/metal.hpp"
 
 // Camera includes
 #include "camera/camera.hpp"
 
-// Utility includes
-#include "shared/utility.hpp"
-
 // Image writer includes
-#include "imageWriters/ppmWriter.hpp"
 #include "imageWriters/imageWriter.hpp"
+#include "imageWriters/ppmWriter.hpp"
 
 namespace Renderer {
-	void singleCoreRender(const int imageWidth, const int imageHeight,
-						  const HittableList& world, const int maxRayDepth,
-						  const Camera& camera, const int samplesPerPixel,
-						  imageWriter& iw, ppmWriter& pw);
-	
-	void multiCoreRender(std::atomic<int>& scanLinesLeft, const int imageWidth,
-						 const int imageHeight, const HittableList& world,
-						 const int maxRayDepth, const Camera& camera, 
-						 const int samplesPerPixel, imageWriter& iw);
-}
+void singleCoreRender(const int imageWidth, const int imageHeight,
+                      const HittableList& world, const int maxRayDepth,
+                      const Camera& camera, const int samplesPerPixel,
+                      imageWriter& iw, ppmWriter& pw);
+
+void multiCoreRender(std::atomic<int>& scanLinesLeft, const int imageWidth,
+                     const int imageHeight, const HittableList& world,
+                     const int maxRayDepth, const Camera& camera,
+                     const int samplesPerPixel, imageWriter& iw);
+}  // namespace Renderer
--- Celerity/src/renderer/renderer.cxx
+++ Celerity/src/renderer/renderer.cxx
@@ -1,93 +1,98 @@
 #include "renderer/renderer.hpp"
 
+#include <syncstream>
+
+#include "materials/material.hpp"
+
 namespace Renderer {
-	color rayColor(const ray& r, const HittableList& world, int depth) {
-		// If we've exceeded the ray bounce limit, no more light is gathered.
-		if (depth <= 0) {
-			return color(0, 0, 0);
-		}
+color rayColor(const ray& r, const HittableList& world, int depth) {
+    // If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth <= 0) {
+        return color(0, 0, 0);
+    }
 
-		HitRecord record;
+    HitRecord record;
 
-		if (world.hit(r, 0.001, infinity, record)) {
-			//TODO: Investigate whether it is worth it to make the tolerance customizable.
-			ray scattered;
-			color attenuation;
+    if (world.hit(r, 0.001, infinity, record)) {
+        // TODO: Investigate whether it is worth it to make the tolerance
+        // customizable.
+        ray scattered;
+        color attenuation;
 
-			if (record.material->scatter(r, record, attenuation, scattered)) {
-				return attenuation * rayColor(scattered, world, depth - 1);
-			}
+        if (record.material->scatter(r, record, attenuation, scattered)) {
+            return attenuation * rayColor(scattered, world, depth - 1);
+        }
 
-			return color(0, 0, 0);
-		}
+        return color(0, 0, 0);
+    }
 
-		vec3 unitDirection = vec3::unitVector(r.direction());
-		//normalizing makes all the coordinates vary from [-1, 1] (inclusive)
+    vec3 unitDirection = vec3::unitVector(r.direction());
+    // normalizing makes all the coordinates vary from [-1, 1] (inclusive)
 
-		auto t = 0.5 * (unitDirection.y() + 1.0);
-		//This is a trick to make the y value vary from [0, 1]
+    auto t = 0.5 * (unitDirection.y() + 1.0);
+    // This is a trick to make the y value vary from [0, 1]
 
-		return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
-		//This is a linear combination of the start and end colors to make a
-		//smooth and linear color gradient.
-	}
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+    // This is a linear combination of the start and end colors to make a
+    // smooth and linear color gradient.
+}
 
-	void singleCoreRender(const int imageWidth, const int imageHeight,
-						  const HittableList& world, const int maxRayDepth,
-						  const Camera& camera, const int samplesPerPixel,
-						  imageWriter& iw, ppmWriter& pw) {
-		for (int j = imageHeight - 1; j >= 0; --j) {
-			std::clog << "\rScanlines remaining: " << j << " " << std::flush;
+void singleCoreRender(const int imageWidth, const int imageHeight,
+                      const HittableList& world, const int maxRayDepth,
+                      const Camera& camera, const int samplesPerPixel,
+                      imageWriter& iw, ppmWriter& pw) {
+    for (int j = imageHeight - 1; j >= 0; --j) {
+        std::clog << "\rScanlines remaining: " << j << " " << std::flush;
 
-			for (int i = 0; i < imageWidth; ++i) {
-				color pixelColor;
-				for (int s = 0; s < samplesPerPixel; ++s) {
-					auto u = (i + randomDouble()) / (imageWidth - 1);
-					auto v = (j + randomDouble()) / (imageHeight - 1);
+        for (int i = 0; i < imageWidth; ++i) {
+            color pixelColor;
+            for (int s = 0; s < samplesPerPixel; ++s) {
+                auto u = (i + randomDouble()) / (imageWidth - 1);
+                auto v = (j + randomDouble()) / (imageHeight - 1);
 
-					ray r = camera.getRay(u, v);
+                ray r = camera.getRay(u, v);
 
-					pixelColor += rayColor(r, world, maxRayDepth);
-				}
+                pixelColor += rayColor(r, world, maxRayDepth);
+            }
 
-				pixelColor.combine(samplesPerPixel);
+            pixelColor.combine(samplesPerPixel);
 
-				pw.writeToBuffer(pixelColor);
-				iw.writeToPNGBuffer(pixelColor);
-				iw.writeToJPGBuffer(pixelColor);
-			}
-		}
-	}
+            pw.writeToBuffer(pixelColor);
+            iw.writeToPNGBuffer(pixelColor);
+            iw.writeToJPGBuffer(pixelColor);
+        }
+    }
+}
 
-	void multiCoreRender(std::atomic<int>& scanLinesLeft, const int imageWidth,
-						 const int imageHeight, const HittableList& world,
-						 const int maxRayDepth, const Camera& camera,
-						 const int samplesPerPixel, imageWriter& iw) {
-		while (scanLinesLeft >= 0) {
-			int currentImageRow = scanLinesLeft;
-			scanLinesLeft--;
-			int bufferRow = imageHeight - currentImageRow - 1;
+void multiCoreRender(std::atomic<int>& scanLinesLeft, const int imageWidth,
+                     const int imageHeight, const HittableList& world,
+                     const int maxRayDepth, const Camera& camera,
+                     const int samplesPerPixel, imageWriter& iw) {
+    while (scanLinesLeft >= 0) {
+        int currentImageRow = scanLinesLeft;
+        scanLinesLeft--;
+        int bufferRow = imageHeight - currentImageRow - 1;
 
-			std::osyncstream syncedLog(std::clog); //TODO: Make this optional.
-			syncedLog << "\rScanlines remaining: " << currentImageRow << " " << std::flush;
+        std::osyncstream syncedLog(std::clog);  // TODO: Make this optional.
+        syncedLog << "\rScanlines remaining: " << currentImageRow << " "
+                  << std::flush;
 
-			for (int i = 0; i < imageWidth; ++i) {
-				color pixelColor;
-				for (int s = 0; s < samplesPerPixel; ++s) {
-					auto u = (i + randomDouble()) / (imageWidth - 1);
-					auto v = (currentImageRow + randomDouble()) / (imageHeight - 1);
+        for (int i = 0; i < imageWidth; ++i) {
+            color pixelColor;
+            for (int s = 0; s < samplesPerPixel; ++s) {
+                auto u = (i + randomDouble()) / (imageWidth - 1);
+                auto v = (currentImageRow + randomDouble()) / (imageHeight - 1);
 
-					ray r = camera.getRay(u, v);
+                ray r = camera.getRay(u, v);
 
-					pixelColor += rayColor(r, world, maxRayDepth);
-				}
+                pixelColor += rayColor(r, world, maxRayDepth);
+            }
 
-				pixelColor.combine(samplesPerPixel);
+            pixelColor.combine(samplesPerPixel);
 
-				iw.writeToJPGBuffer(3 * (bufferRow * imageWidth + i), pixelColor);
-				iw.writeToPNGBuffer(3 * (bufferRow * imageWidth + i), pixelColor);
-			}
-
-		}
-	}
-} // Renderer namespace
+            iw.writeToJPGBuffer(3 * (bufferRow * imageWidth + i), pixelColor);
+            iw.writeToPNGBuffer(3 * (bufferRow * imageWidth + i), pixelColor);
+        }
+    }
+}
+}  // namespace Renderer
