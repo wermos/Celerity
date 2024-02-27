1d4fd993aee790aa7d139d24e1dd0765a87335b3
wermos <63574588+wermos@users.noreply.github.com>
2024-02-27 12:03:29 +0530

    Reformatted the code and made a minor change.

--- Celerity/include/materials/dielectric.hpp
+++ Celerity/include/materials/dielectric.hpp
@@ -2,29 +2,27 @@
 
 #include <cmath>
 
-#include "float.hpp"
-
-#include "shared/hitRecord.hpp"
-#include "materials/material.hpp"
-
 #include "color.hpp"
+#include "float.hpp"
+#include "materials/material.hpp"
+#include "shared/hitRecord.hpp"
 
 class Dielectric : public Material {
-	public:
-		constexpr Dielectric(Float refractiveIndex) noexcept
-			: m_refractiveIndex(refractiveIndex) {}
+   public:
+    constexpr Dielectric(Float refractiveIndex) noexcept
+        : m_refractiveIndex(refractiveIndex) {}
 
-		virtual bool scatter(
-			const ray& in, const HitRecord& rec, color& attenuation, ray& scattered
-		) const override;
+    virtual bool scatter(const ray& in, const HitRecord& rec,
+                         color& attenuation, ray& scattered) const override;
 
-	private:
-		Float m_refractiveIndex;
+   private:
+    Float m_refractiveIndex;
 
-		static Float reflectance(Float cosine, Float refractiveIndexRatio) {
-			// Use Schlick's approximation for the Fresnel equations for reflectance.
-			auto r_0 = (1 - refractiveIndexRatio) / (1 + refractiveIndexRatio);
-			r_0 = r_0 * r_0;
-			return r_0 + (1 - r_0) * std::pow((1 - cosine), 5);
-		}
+    static Float reflectance(Float cosine, Float refractiveIndexRatio) {
+        // Use Schlick's approximation for the Fresnel equations for
+        // reflectance.
+        auto r_0 = (1 - refractiveIndexRatio) / (1 + refractiveIndexRatio);
+        r_0 *= r_0;
+        return r_0 + (1 - r_0) * std::pow((1 - cosine), 5);
+    }
 };
