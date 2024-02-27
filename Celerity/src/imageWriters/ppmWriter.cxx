6399d33f883b46baf0ac4eb5aa3d98ef4832ddda
wermos <63574588+wermos@users.noreply.github.com>
2024-02-27 11:57:01 +0530

    Reformatted the code and made minor changes to the member variables of the ppm image writer class.

--- Celerity/src/imageWriters/ppmWriter.cxx
+++ Celerity/src/imageWriters/ppmWriter.cxx
@@ -1,24 +1,24 @@
 #include "imageWriters/ppmWriter.hpp"
 
-ppmWriter::ppmWriter(int imageWidth, int imageHeight) : m_imageWidth(imageWidth),
-	m_imageHeight(imageHeight) {
-	m_file.open(m_fileName, std::ofstream::out | std::ofstream::trunc);
+ppmWriter::ppmWriter(std::size_t imageWidth, std::size_t imageHeight)
+    : m_imageWidth(imageWidth), m_imageHeight(imageHeight) {
+    m_file.open(m_fileName, std::ofstream::out | std::ofstream::trunc);
 
-	if (m_file.good()) {
-		m_file << "P3\n" << imageWidth << " " << imageHeight << "\n255\n";
-	} else {
-		m_useStandardOutput = true;
-		std::cerr << "Error opening file. Switching to standard output.\n";
-		std::cout << "P3\n" << imageWidth << " " << imageHeight << "\n255\n";
-	}
-	/**
-	 * P3
-	 * <width> <height>
-	 * <max value of color>
-	 *
-	 * where "P3" tells the viewer that the colors are in ASCII
-	 * <width> tells the viewer how many columns there are in the picture,
-	 * <height> tells the viewer how many rows, and
-	 * <max value of color> tells the viewer how to normalize the color values
-	 */
+    if (m_file.good()) {
+        m_file << "P3\n" << imageWidth << " " << imageHeight << "\n255\n";
+    } else {
+        m_useStandardOutput = true;
+        std::cerr << "Error opening file. Switching to standard output.\n";
+        std::cout << "P3\n" << imageWidth << " " << imageHeight << "\n255\n";
+    }
+    /**
+     * P3
+     * <width> <height>
+     * <max value of color>
+     *
+     * where "P3" tells the viewer that the colors are in ASCII
+     * <width> tells the viewer how many columns there are in the picture,
+     * <height> tells the viewer how many rows, and
+     * <max value of color> tells the viewer how to normalize the color values
+     */
 }
