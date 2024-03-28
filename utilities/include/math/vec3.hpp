1af5798cbe2c9986bd5a3aa1b1bfff1dfa9b51d0
wermos <63574588+wermos@users.noreply.github.com>
2024-03-28 19:41:01 +0530

    Removed the usage of `xsimd` in the vec3 code.

--- utilities/include/math/vec3.hpp
+++ utilities/include/math/vec3.hpp
@@ -1,5 +1,4 @@
-#ifndef VEC3_HPP
-#define VEC3_HPP
+#pragma once
 
 /* STL includes */
 #include <cmath>
@@ -9,13 +8,10 @@
 #include <numbers>
 #include <type_traits>
 
-/* Preprocessor macro file includes */
-#include "constexpr.hpp" // for the CMATH_CONSTEXPR macro
-
 /* color header include */
 #include "color.hpp"
 
-#include <xmmintrin.h>
+#include <immintrin.h>
 
 // It needs to be aligned on 128-bit boundaries because we are going to load the components into
 // SSE registers for addition and stuff
@@ -24,11 +20,6 @@ class alignas(16) vec3 {
 		constexpr vec3() noexcept : m_e{0, 0, 0} {}
 		constexpr vec3(float e0, float e1, float e2) noexcept : m_e{e0, e1, e2} {}
 		constexpr vec3(const color& c) noexcept : m_e{c.raw_r(), c.raw_b(), c.raw_g()} {}
-
-		template <typename T = float, typename Arch>
-		constexpr vec3(xsimd::batch<T, Arch> reg) noexcept {
-			reg.store_unaligned(m_e);
-		}
 
 		constexpr float x() const {
 			return m_e[0];
@@ -48,9 +39,20 @@ class alignas(16) vec3 {
 				return {-m_e[0], -m_e[1], -m_e[2]};
 			} else {
 				// runtime branch
-				auto op = xsimd::batch<float, xsimd::sse4_2>::load_aligned(m_e);
 
-				return {xsimd::neg(op)};
+				__m128 data = _mm_load_ps(m_e);
+
+				// Create a __m128 with all bits set to represent -0.0f
+				__m128 negZero = _mm_set1_ps(-0.0f);
+
+				// Negate the data
+				__m128 negData = _mm_xor_ps(data, negZero);
+
+
+				// auto op = xsimd::batch<float, xsimd::sse4_2>::load_aligned(m_e);
+
+				// return {xsimd::neg(op)};
+				return {negData};
 			}
 		}
 
@@ -70,12 +72,19 @@ class alignas(16) vec3 {
 				u.m_e[2] += v.m_e[2];
 			} else {
 				// runtime branch
-				auto op1 = xsimd::batch<float, xsimd::sse4_2>::load_aligned(u.m_e);
-				auto op2 = xsimd::batch<float, xsimd::sse4_2>::load_aligned(v.m_e);
 
-				op1 += op2;
+				__m128 op1 = _mm_load_ps(u.m_e);
+				__m128 op2 = _mm_load_ps(v.m_e);
+				// auto op1 = xsimd::batch<float, xsimd::sse4_2>::load_aligned(u.m_e);
+				// auto op2 = xsimd::batch<float, xsimd::sse4_2>::load_aligned(v.m_e);
 
-				op1.store_aligned(u.m_e);
+				// op1 += op2;
+
+				// op1.store_aligned(u.m_e);
+
+				op1 = _mm_add_ps(op1, op2);
+				_mm_store_ps(u.m_e, op1);
+
 			}
 
 			return u;
@@ -89,12 +98,18 @@ class alignas(16) vec3 {
 				u.m_e[2] -= v.m_e[2];
 			} else {
 				// runtime branch
-				auto op1 = xsimd::batch<float, xsimd::sse4_2>::load_aligned(u.m_e);
-				auto op2 = xsimd::batch<float, xsimd::sse4_2>::load_aligned(v.m_e);
 
-				op1 -= op2;
+				__m128 op1 = _mm_load_ps(u.m_e);
+				__m128 op2 = _mm_load_ps(v.m_e);
+				// auto op1 = xsimd::batch<float, xsimd::sse4_2>::load_aligned(u.m_e);
+				// auto op2 = xsimd::batch<float, xsimd::sse4_2>::load_aligned(v.m_e);
 
-				op1.store_aligned(u.m_e);
+				// op1 -= op2;
+
+				// op1.store_aligned(u.m_e);
+
+				op1 = _mm_sub_ps(op1, op2);
+				_mm_store_ps(u.m_e, op1);
 			}
 
 			return u;
@@ -108,12 +123,19 @@ class alignas(16) vec3 {
 				v.m_e[2] *= t;
 			} else {
 				// runtime branch
-				xsimd::batch<float, xsimd::sse4_2> op1(t);
-				auto op2 = xsimd::batch<float, xsimd::sse4_2>::load_aligned(v.m_e);
 
-				op1 *= op2;
+				__m128 op1 = _mm_set1_ps(t);
+				__m128 op2 = _mm_load_ps(v.m_e);
+				// xsimd::batch<float, xsimd::sse4_2> op1(t);
+				// auto op2 = xsimd::batch<float, xsimd::sse4_2>::load_aligned(v.m_e);
 
-				op1.store_aligned(v.m_e);
+				// op1 *= op2;
+
+				// op1.store_aligned(v.m_e);
+
+				op1 = _mm_mul_ps(op1, op2);
+
+				_mm_store_ps(v.m_e, op1);
 			}
 
 			return v;
@@ -177,18 +199,35 @@ class alignas(16) vec3 {
 		}
 
 		constexpr static vec3 cross(const vec3& u, const vec3& v) {
-			return {u.m_e[1] * v.m_e[2] - u.m_e[2] * v.m_e[1],
+			if (std::is_constant_evaluated()) {
+				// constexpr branch
+				return {u.m_e[1] * v.m_e[2] - u.m_e[2] * v.m_e[1],
 					u.m_e[2] * v.m_e[0] - u.m_e[0] * v.m_e[2],
 					u.m_e[0] * v.m_e[1] - u.m_e[1] * v.m_e[0]};
+			} else {
+				// runtime branch
+				//
+				// inspired by:
+				// https://geometrian.com/programming/tutorials/cross-product/index.php
+				
+				__m128 vec0 = _mm_load_ps(u.m_e);
+				__m128 vec1 = _mm_load_ps(v.m_e);
+
+				__m128 tmp0 = _mm_shuffle_ps(vec0, vec0, _MM_SHUFFLE(3, 0, 2, 1));
+				__m128 tmp1 = _mm_shuffle_ps(vec1, vec1, _MM_SHUFFLE(3, 1, 0, 2));
+				__m128 tmp2 = _mm_mul_ps(tmp0, vec1);
+				__m128 tmp3 = _mm_mul_ps(tmp0, tmp1);
+				__m128 tmp4 = _mm_shuffle_ps(tmp2, tmp2, _MM_SHUFFLE(3, 0, 2, 1));
+
+				return {_mm_sub_ps(tmp3, tmp4)};
+			}
 		}
 
-		CMATH_CONSTEXPR static vec3 normalize(vec3 v) {
-			// This function needs the CMATH_CONSTEXPR macro because it calls a function which uses a cmath
-			// function under the hood.
+		constexpr static vec3 normalize(vec3 v) {
 			return v / v.length();
 		}
 
-		CMATH_CONSTEXPR float length() const {
+		constexpr float length() const {
 			return std::sqrt(length_squared());
 		}
 
@@ -218,14 +257,10 @@ class alignas(16) vec3 {
 			float thetaRad = randomfloat(0, 1) * std::numbers::pi;
 			float phiRad = randomfloat(0, 2) * std::numbers::pi;
 
-#ifdef USE_DOUBLE_AS_FLOAT_TYPE
-			return {sin(thetaRad) * cos(phiRad), sin(thetaRad) * sin(phiRad), cos(thetaRad)};
-#else
 			return {sinf(thetaRad) * cosf(phiRad), sinf(thetaRad) * sinf(phiRad), cosf(thetaRad)};
-#endif
 		}
 
-		CMATH_CONSTEXPR bool nearZero() const {
+		constexpr bool nearZero() const {
 			// Returns true if the vector is close to zero in all dimensions.
 			const float threshold = 1e-8;
 			return (std::fabs(m_e[0]) < threshold) && (std::fabs(m_e[1]) < threshold) && (std::fabs(m_e[2]) < threshold);
@@ -262,22 +297,21 @@ class alignas(16) vec3 {
 		}
 
 	private:
-		float m_e[3];
-
 		static float randomfloat(float min, float max) {
 			// Returns a random real in [min, max].
 			static std::uniform_real_distribution<float> distribution(min, std::nextafter(max, std::numeric_limits<float>::infinity()));
-#ifdef USE_DOUBLE_AS_FLOAT_TYPE
-			static std::mt19937_64 generator;
-#else
+			
 			static std::mt19937 generator;
-#endif
 
 			return distribution(generator);
 		}
+	
+		constexpr vec3(__m128 reg) noexcept {
+			_mm_store_ps(m_e, reg);
+		}
+	
+		float m_e[3];
 };
 
 // Type aliases for vec3
 using point3 = vec3;    // 3D point
-
-#endif // VEC3_HPP
